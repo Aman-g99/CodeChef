eee28cd3ba0e3a97d3164f8f6202e36ea45df00e
Aman Garg <32516081+Aman-g99@users.noreply.github.com>
2019-12-25 18:17:57 +0530

    update
    update

--- NOV19B/CAMC.cpp
+++ NOV19B/CAMC.cpp
@@ -7,40 +7,52 @@ int main(){
 	{
 		int n,m;
 		cin>>n>>m;
-		int arr[n];
+		vector<pair<int,int>> vec;
 		for(int j=0; j<n;j++){
-			cin>>arr[j];
+			int temp;cin>>temp;
+			vec.push_back(make_pair(temp,(j%m)+1));
 		}
+		sort(vec.begin(),vec.end());
+		for(int j=0; j<n;j++){
+			cout<<vec[j].first<<"->"<<vec[j].second<<" ";
+		}
+		cout<<"\n";
+
+		  
 
 		int mn=INT_MAX,count=1;
 
-		for(int j=0;j<n-1;j++){
-			for(int k=1;k<=n;k++){
-				if(j+k<n){
-					if(count<m){
-						//cout<<arr[j]<<" "<<arr[j+k]<<"\n";
-						mn=min(mn,abs(arr[j]-arr[j+k]));
-						//if(abs(arr[j]-arr[j+k])<mn){
-						//	mn = abs(arr[j]-arr[j+k]);
-						//}
+		// for(int j=0;j<n-1;j++){
+		// 	int maxi=arr[j],mini=arr[j];
+		// 	for(int k=1;k<=n;k++){
+		// 		if(j+k<n){
+		// 			if(count<m){
 						
-						count++;
-					}
-					else {
-						count=1;
-					}
+		// 				if(maxi<arr[j+k]) maxi = arr[j+k];
+		// 				if(mini>arr[j+k]) mini = arr[j+k];
+		// 				//cout<<arr[j]<<" "<<arr[j+k]<<"\n";
+		// 				mn=min(mn,abs(maxi-mini));
+		// 				//if(abs(arr[j]-arr[j+k])<mn){
+		// 				//	mn = abs(arr[j]-arr[j+k]);
+		// 				//}
+						
+		// 				count++;
+		// 			}
+		// 			else {
+		// 				count=1;
+		// 			}
 					
-				}
-				else {
-					//cout<<k<<"\n";
-					count=1;
-					break;
-				}
+		// 		}
+		// 		else {
+		// 			//cout<<k<<"\n";
+		// 			count=1;
+		// 			break;
+		// 		}
 
-			}
+		// 	}
 
-		}
-		cout<<mn<<"\n";
+		// }
+		// cout<<mn<<"\n";
 
 	}
 }
